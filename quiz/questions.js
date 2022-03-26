2289dbb357de1fee9765dbe7e9fae5d1e2f35927
kanami <mimimimi12611@gmail.com>
2022-03-26 15:20:48 +0900

    test

---
+++ quiz/questions.js
@@ -0,0 +1,16 @@
+//const answersList = document.querySelectorAll('ol.anawers li');
+
+//answersList.forEach(li => li.addEventListener('click', checkClickedAnswer));
+
+//function checkClickedAnswer() {
+    //alert('clicked!');
+
+    //document.querySelector('div#section-correct-answer').style.display = 'block';
+//}
+
+function showAlert() {
+    alert('イッヌ');
+}
+let button = document.querySelector('button#btn');
+
+button.addEventListener('click', showAlert);
